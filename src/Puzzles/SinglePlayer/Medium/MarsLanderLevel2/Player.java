0e4242aa39e79132c3b237f5e213e2a7382bce96
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2016-05-10 18:22:33 +0300

    Solving puzzle Mars Lander - Level 2

--- src/Puzzles/SinglePlayer/Medium/MarsLanderLevel2/Player.java
+++ src/Puzzles/SinglePlayer/Medium/MarsLanderLevel2/Player.java
@@ -1,5 +1,7 @@
 package Puzzles.SinglePlayer.Medium.MarsLanderLevel2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -8,13 +10,21 @@ import java.util.Scanner;
  **/
 class Player {
 
+    private static int plainMinX, plainMaxX, plainCenter;
+
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int surfaceN = in.nextInt(); // the number of points used to draw the surface of Mars.
+        List<Point> surfaceCoords = new ArrayList<>(surfaceN);
         for (int i = 0; i < surfaceN; i++) {
             int landX = in.nextInt(); // X coordinate of a surface point. (0 to 6999)
             int landY = in.nextInt(); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+            surfaceCoords.add(new Point(landX, landY));
         }
+
+        // Find plain surface coordinates
+        fingPlaneSurface(surfaceCoords);
+        plainCenter = (plainMinX + plainMaxX) / 2;
 
         // game loop
         while (true) {
@@ -28,10 +38,68 @@ class Player {
 
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
+            System.err.println("min: " + plainMinX + " max: " + plainMaxX);
+            int pow = 0;
 
+            if (Math.abs(hSpeed) < 70 && Y < 2200) {
+                if (Y < 200) {
+                    action(0, 4);
+                } else {
+                    if (plainMinX < X) {
+                        action(20, 4);
+                    } else {
+                        action(-20, 4);
+                    }
+                }
+            }
+            else {
+                if (Math.abs(rotate) < 40) {
+                    pow = 0;
+                } else {
+                    pow = 4;
+                }
+                int direction = hSpeed > 0 ? -1 : 1;
+
+                if (plainMinX < X) {
+                    action(50 * direction, pow);
+                } else {
+                    action(-50 * direction, pow);
+                }
+            }
+
+            /*if (X - plainCenter < 0) {
+                if (angle )
+                angle = angle * -1;
+            }*/
+
+            /*if (X < plainCenter) {
+                if (X < plainCenter - 1200) {
+                    if (X > plainCenter - 500) {
+                        if (Y > 300) {
+                            System.out.println("22 4");
+                        } else {
+                            System.out.println("0 4");
+                        }
+                    } else {
+                        if (Y > 300) {
+                            System.out.println("-22 3");
+                        } else {
+                            System.out.println("0 4");
+                        }
+                    }
+                } else {
+                    if (Y > 700) {
+                        System.out.println("22 4");
+                    } else {
+                        System.out.println("0 4");
+                    }
+                }
+            } else if (X > plainCenter) {
+                System.out.println("22 3");
+            }*/
 
             // rotate power. rotate is the desired rotation angle. power is the desired thrust power.
-            if (X < 3500) {
+            /*if (X < 3500) {
                 System.out.println("-25 3");
             } else {
                 if (Y > 700) {
@@ -39,8 +107,52 @@ class Player {
                 } else {
                     System.out.println("0 4");
                 }
-            }
+            }*/
 
+
+        }
+    }
+
+    private static void fingPlaneSurface(List<Point> surfaceCoords) {
+        boolean isFoundPlainMinX = false, isFoundPlainMaxX = false;
+        for (int i = 0; i < surfaceCoords.size(); i++) {
+            if (i + 1 != surfaceCoords.size()) {
+                if (surfaceCoords.get(i).y == surfaceCoords.get(i + 1).y && !isFoundPlainMinX) {
+                    isFoundPlainMinX = true;
+                    plainMinX = surfaceCoords.get(i).x;
+                }
+                if (isFoundPlainMinX && !isFoundPlainMaxX && surfaceCoords.get(i).y != surfaceCoords.get(i + 1).y) {
+                    isFoundPlainMaxX = true;
+                    plainMaxX = surfaceCoords.get(i).x;
+                }
+            }
+        }
+    }
+
+    private static void action(int angle, int power) {
+        System.out.println(angle + " " + power);
+    }
+
+    private static class Point {
+
+        final int x;
+        final int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        private static long point(int x, int y) {
+            return (((long) x) << 32) | y;
+        }
+
+        private static int x(long point) {
+            return (int) (point >> 32);
+        }
+
+        private static int y(long point) {
+            return (int) point;
         }
     }
 }
